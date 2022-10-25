14fb7456eaaacb6dc15fd72a3de498beb02752fb
Maria Azul Rojas <azulm.rojas@gmail.com>
2022-10-24 21:06:08 -0300

    Update spotify_db.sql

--- spotify_db.sql
+++ spotify_db.sql
@@ -1,46 +1,65 @@
 CREATE DATABASE SPOTIFY;
  
- CREATE TABLE indice (
-   id_cancion int,
-   nombre_cancion varchar(50)
+ 
+ 
+ CREATE TABLE IF NOT EXISTS indice(
+   id_cancion int NOT NULL AUTO_INCREMENT,
+   nombre_cancion varchar(50) NOT NULL,
+   PRIMARY KEY (id_cancion)
    );
    
- CREATE TABLE artista (
-   nombre_artista varchar(50),
-   id_cancion int,
+ CREATE TABLE  IF NOT EXISTS artista (
+   nombre_artista varchar(50) NOT NULL,
+   id_cancion int NOT NULL,
+   PRIMARY KEY (nombre_artista),
+   FOREIGN KEY (id_cancion) REFERENCES indice (id_cancion)
    );
    
- CREATE TABLE cancion (
-    nombre_cancion varchar(50),
-    id_cancion int,
-    genero_musical varchar(50),
-    duracion int,
-    nombre_artista varchar(50),
-    year int
-    );
    
-  CREATE TABLE genero (
-     genero_musical varchar(50),
-     id_cancion int,
-     nombre_cancion varchar(50)
+  CREATE TABLE IF NOT EXISTS  genero (
+     genero_musical varchar(50) NOT NULL,
+     id_cancion int NOT NULL,
+     nombre_cancion varchar(50) NOT NULL,
+     PRIMARY KEY (genero_musical),
+     FOREIGN KEY (id_cancion) REFERENCES indice (id_cancion)
      );
     
-  CREATE TABLE popularidad (
-    popularidad int,
-    id_cancion int,
-    nombre_cancion varchar(50)
+  CREATE TABLE IF NOT EXISTS  popularidad (
+    popularidad int NOT NULL,
+    id_cancion int NOT NULL,
+    nombre_cancion varchar(50) NOT NULL,
+    PRIMARY KEY (popularidad),
+    FOREIGN KEY (id_cancion) REFERENCES indice (id_cancion)
     );
     
-  CREATE TABLE duracion (
-     duracion int,
-     id_cancion int,
+  CREATE TABLE IF NOT EXISTS duracion(
+     duracion int NOT NULL,
+     id_cancion int NOT NULL,
+     PRIMARY KEY (duracion),
+     FOREIGN KEY (id_cancion) REFERENCES indice (id_cancion)
      );
      
-  CREATE TABLE Year (
-    Year int,
-    id_cancion int,
-    nombre_cancion varchar(50),
-    nombre_artista varchar(50),
-    genero_musical varchar(50)
+  CREATE TABLE IF NOT EXISTS year(
+    year int NOT NULL,
+    id_cancion int NOT NULL,
+    nombre_cancion varchar(50) NOT NULL,
+    nombre_artista varchar(50) NOT NULL,
+    genero_musical varchar(50) NOT NULL,
+    PRIMARY KEY (year),
+    FOREIGN KEY (nombre_artista) REFERENCES artista (nombre_artista),
+    FOREIGN KEY (genero_musical) REFERENCES genero (genero_musical)
     );
-  
+    
+     CREATE TABLE  IF NOT EXISTS  cancion (
+    nombre_cancion varchar(50) NOT NULL,
+    id_cancion int NOT NULL,
+    genero_musical varchar(50) NOT NULL,
+    duracion int NOT NULL,
+    nombre_artista varchar(50) NOT NULL,
+    year int NOT NULL,
+    PRIMARY KEY (nombre_cancion),
+    FOREIGN KEY (id_cancion) REFERENCES indice (id_cancion),
+    FOREIGN KEY (genero_musical) REFERENCES genero (genero_musical),
+    FOREIGN KEY (duracion) REFERENCES duracion (duracion),
+    FOREIGN KEY (nombre_artista) REFERENCES artista (nombre_artista)
+    );
