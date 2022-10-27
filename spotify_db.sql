2c932208a64c2e8e3523e1ea01b50b823c5beeee
Maria Azul Rojas <azulm.rojas@gmail.com>
2022-10-26 23:06:52 -0300

    Update spotify_db.sql

--- spotify_db.sql
+++ spotify_db.sql
@@ -1,65 +1,80 @@
 CREATE DATABASE SPOTIFY;
- 
- 
- 
- CREATE TABLE IF NOT EXISTS indice(
-   id_cancion int NOT NULL AUTO_INCREMENT,
-   nombre_cancion varchar(50) NOT NULL,
-   PRIMARY KEY (id_cancion)
-   );
-   
- CREATE TABLE  IF NOT EXISTS artista (
-   nombre_artista varchar(50) NOT NULL,
-   id_cancion int NOT NULL,
-   PRIMARY KEY (nombre_artista),
-   FOREIGN KEY (id_cancion) REFERENCES indice (id_cancion)
-   );
-   
-   
-  CREATE TABLE IF NOT EXISTS  genero (
-     genero_musical varchar(50) NOT NULL,
-     id_cancion int NOT NULL,
-     nombre_cancion varchar(50) NOT NULL,
-     PRIMARY KEY (genero_musical),
-     FOREIGN KEY (id_cancion) REFERENCES indice (id_cancion)
-     );
-    
-  CREATE TABLE IF NOT EXISTS  popularidad (
-    popularidad int NOT NULL,
-    id_cancion int NOT NULL,
-    nombre_cancion varchar(50) NOT NULL,
-    PRIMARY KEY (popularidad),
-    FOREIGN KEY (id_cancion) REFERENCES indice (id_cancion)
-    );
-    
-  CREATE TABLE IF NOT EXISTS duracion(
-     duracion int NOT NULL,
-     id_cancion int NOT NULL,
-     PRIMARY KEY (duracion),
-     FOREIGN KEY (id_cancion) REFERENCES indice (id_cancion)
-     );
      
-  CREATE TABLE IF NOT EXISTS year(
-    year int NOT NULL,
-    id_cancion int NOT NULL,
-    nombre_cancion varchar(50) NOT NULL,
-    nombre_artista varchar(50) NOT NULL,
-    genero_musical varchar(50) NOT NULL,
-    PRIMARY KEY (year),
-    FOREIGN KEY (nombre_artista) REFERENCES artista (nombre_artista),
-    FOREIGN KEY (genero_musical) REFERENCES genero (genero_musical)
-    );
-    
-     CREATE TABLE  IF NOT EXISTS  cancion (
-    nombre_cancion varchar(50) NOT NULL,
-    id_cancion int NOT NULL,
-    genero_musical varchar(50) NOT NULL,
-    duracion int NOT NULL,
-    nombre_artista varchar(50) NOT NULL,
-    year int NOT NULL,
-    PRIMARY KEY (nombre_cancion),
-    FOREIGN KEY (id_cancion) REFERENCES indice (id_cancion),
-    FOREIGN KEY (genero_musical) REFERENCES genero (genero_musical),
-    FOREIGN KEY (duracion) REFERENCES duracion (duracion),
-    FOREIGN KEY (nombre_artista) REFERENCES artista (nombre_artista)
-    );
+    CREATE TABLE `indice` (
+  `Id` int NOT NULL,
+  `Nombre_cancion` varchar(45) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+CREATE TABLE `popularidad` (
+  `popularidad` int NOT NULL,
+  `Id` int NOT NULL,
+  `nombre_cancion` varchar(45) NOT NULL,
+  PRIMARY KEY (`popularidad`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+CREATE TABLE `spotify`.`cancion` (
+  `Id` INT NOT NULL,
+  `nombre_artista` VARCHAR(45) NOT NULL,
+  `nombre_cancion` VARCHAR(45) NOT NULL,
+  `genero_musical` VARCHAR(45) NOT NULL,
+  `año` INT NOT NULL,
+  `duracion` INT NOT NULL,
+  PRIMARY KEY (`nombre_cancion`));
+
+ALTER TABLE `spotify`.`cancion` 
+ADD COLUMN `bpm` INT NOT NULL AFTER `duracion`,
+ADD COLUMN `danceability` INT NOT NULL AFTER `bpm`,
+ADD COLUMN `energia` INT NOT NULL AFTER `danceability`,
+ADD COLUMN `popularidad` INT NOT NULL AFTER `energia`;
+
+
+CREATE TABLE `spotify`.`artista` (
+  `nombre_artista` VARCHAR(45) NOT NULL,
+  `Id` INT NOT NULL,
+  PRIMARY KEY (`nombre_artista`));
+
+
+CREATE TABLE `spotify`.`genero` (
+  `genero_musical` VARCHAR(45) NOT NULL,
+  `Id` INT NOT NULL,
+  `nombre_cancion` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`genero_musical`));
+
+CREATE TABLE `energia` (
+  `energia` int NOT NULL,
+  `Id` int NOT NULL,
+  PRIMARY KEY (`energia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `duracion` (
+  `duracion` int NOT NULL,
+  `Id` int NOT NULL,
+  PRIMARY KEY (`duracion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+CREATE TABLE `danceability` (
+  `danceaility` int NOT NULL,
+  `Id` int NOT NULL,
+  PRIMARY KEY (`danceaility`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `bpm` (
+  `Bpm` int NOT NULL,
+  `Id` int NOT NULL,
+  PRIMARY KEY (`Bpm`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `año` (
+  `año` int NOT NULL,
+  `Id` int NOT NULL,
+  `nombre_cancion` varchar(45) NOT NULL,
+  `nombre_artista` varchar(45) NOT NULL,
+  `genero_musical` varchar(45) NOT NULL,
+  PRIMARY KEY (`año`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
